d70834d88eb24683cdeed4e8e19f43f4611e153f
Guilherme Souza <guilherme.souza19@outlook.com.br>
2017-09-04 00:17:43 -0300

    rebuild

--- TesteUnicamp.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-func imprimeValorDeUmInteiro(valorInteiro : Int){
-    print(valorInteiro*valorInteiro)
-}
-
-imprimeValorDeUmInteiro(valorInteiro: 5)
--- coursera/coursera/NovoArquivo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NovoArquivo.swift
-//  coursera
-//
-//  Created by Guilherme Souza on 8/31/17.
-//  Copyright © 2017 Guilherme Souza. All rights reserved.
-//
-
-import Foundation
